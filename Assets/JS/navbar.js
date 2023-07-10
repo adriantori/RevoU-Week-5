7df72f43584eac6446b0f8302ef70a2b6cd2923e
Adri Antori <adriantori11@gmail.com>
2023-07-10 20:10:16 +0700

    added sidebar

--- Assets/JS/navbar.js
+++ Assets/JS/navbar.js
@@ -2,6 +2,8 @@ let toggleBtn = document.querySelector('.toggleBtn');
 let toggleBtnIcon = document.querySelector('.toggleBtn i');
 let dropdownMenu = document.querySelector('.dropdownMenu');
 
+
+//burger
 toggleBtn.onclick = function(){
     dropdownMenu.classList.toggle('open');
     let isOpen = dropdownMenu.classList.contains('open');
@@ -9,4 +11,41 @@ toggleBtn.onclick = function(){
     toggleBtnIcon.classList = isOpen
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
+}
+
+//generateSideNavActive
+function activeHome() {
+    document.getElementById("navHome").className = "sideNavLink sideNavLinkActive";
+    document.getElementById("navOpening").className = "sideNavLink";
+    document.getElementById("navServices").className = "sideNavLink";
+    document.getElementById("navShowcase").className = "sideNavLink";
+    document.getElementById("navContact").className = "sideNavLink";
+}
+function activeOpening() {
+    document.getElementById("navHome").className = "sideNavLink";
+    document.getElementById("navOpening").className = "sideNavLink sideNavLinkActive";
+    document.getElementById("navServices").className = "sideNavLink";
+    document.getElementById("navShowcase").className = "sideNavLink";
+    document.getElementById("navContact").className = "sideNavLink";
+}
+function activeServices() {
+    document.getElementById("navHome").className = "sideNavLink";
+    document.getElementById("navOpening").className = "sideNavLink";
+    document.getElementById("navServices").className = "sideNavLink sideNavLinkActive";
+    document.getElementById("navShowcase").className = "sideNavLink";
+    document.getElementById("navContact").className = "sideNavLink";
+}
+function activeShowcase() {
+    document.getElementById("navHome").className = "sideNavLink";
+    document.getElementById("navOpening").className = "sideNavLink";
+    document.getElementById("navServices").className = "sideNavLink";
+    document.getElementById("navShowcase").className = "sideNavLink sideNavLinkActive";
+    document.getElementById("navContact").className = "sideNavLink";
+}
+function activeContact() {
+    document.getElementById("navHome").className = "sideNavLink";
+    document.getElementById("navOpening").className = "sideNavLink";
+    document.getElementById("navServices").className = "sideNavLink";
+    document.getElementById("navShowcase").className = "sideNavLink";
+    document.getElementById("navContact").className = "sideNavLink sideNavLinkActive";
 }
